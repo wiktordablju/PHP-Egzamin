a0abcd140d9b8f980e9e35b4674fe073f9b46da1
dablju <82543221+wiktordablju@users.noreply.github.com>
2023-02-15 11:40:36 +0100

    Add files via upload

---
+++ Egzamin1/script.js
@@ -0,0 +1,7 @@
+function oblicz() {
+	const wynik = document.getElementById("wynik");
+	const powierzchnia = +document.getElementById("powierzchnia").value;
+	wynik.innerHTML =
+		"Liczba jednolitrowych puszek farby potrzebnych do pomalowania wynosi: " +
+		Math.ceil(powierzchnia / 4);
+}
