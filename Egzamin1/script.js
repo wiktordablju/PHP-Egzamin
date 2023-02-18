cad89ee2b59743482519666a90b8174624bb10c4
dablju <82543221+wiktordablju@users.noreply.github.com>
2023-02-18 17:38:27 +0100

    Usuniecie Egzamin1
    Przenoszenie folderu do innego repo

--- Egzamin1/script.js
+++
@@ -1,7 +0,0 @@
-function oblicz() {
-	const wynik = document.getElementById("wynik");
-	const powierzchnia = +document.getElementById("powierzchnia").value;
-	wynik.innerHTML =
-		"Liczba jednolitrowych puszek farby potrzebnych do pomalowania wynosi: " +
-		Math.ceil(powierzchnia / 4);
-}
